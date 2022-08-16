16b3e07d0fb29cc9145cd47ffd4487bd7d223810
Céline Sèbe <celine.sebe@gmail.com>
2022-08-16 23:08:09 +0200

    user created in Database

--- front/src/components/SignForm/SignUp.js
+++ front/src/components/SignForm/SignUp.js
@@ -1,7 +1,7 @@
 import  React, { useState } from 'react';
 import styled from 'styled-components';
 import colors from '../../utils/style/colors';
-import axios from 'react';
+import axios from 'axios';
 
 const Signup = styled.div`
 

15f3f9abf8a63e4b8f613421afd6eddb78dcc036
Céline Sèbe <celine.sebe@gmail.com>
2023-02-09 23:39:17 +0100

    setIsModif ok formModif

--- front/src/components/ButtonModify/index.js
+++ front/src/components/ButtonModify/index.js
@@ -2,71 +2,296 @@ import React from 'react'
 import styled from 'styled-components'
 import axios from 'axios'
 import { useState } from 'react'
+import colors from '../../utils/style/colors'
+import assets from '../../assets/user-solid.svg'
 
 const Modif = styled.div`
+  opacity: 1;
+`
+
+// const ContainerCreatePubli = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   background-color: white;
+//   height: 100%;
+//   width: 100%;
+//   border-radius: 20px;
+//   margin: 5px 0;
+//   border: solid 2px ${colors.secondary};
+// `
+// const FormPost = styled.form`
+//   height: 100%;
+//   width: 100%;
+// `
+
+// const ContainerPublication = styled.div`
+//   display: flex;
+//   justify-content: space-between;
+//   align-items: center;
+//   width: 90%;
+// `
+
+// const FormStyle = styled.div`
+//   border-bottom: solid 2px ${colors.secondary};
+//   width: 80%;
+// `
+
+// const InputStyleDescription = styled.input`
+//   border: none;
+//   padding: 10px 0px 15px 15px;
+//   height: 90%;
+//   width: 100%;
+//   &::placeholder {
+//     font-size: 14px;
+//   }
+//   &:focus {
+//     outline: 1px solid white;
+//   }
+// `
+
+// const ProfilImgContainer = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   border-radius: 50px;
+//   width: 60px;
+//   height: 55px;
+//   margin: 45px 0px 0px 20px;
+// `
+
+// const PostImg = styled.img`
+//   width: 100%;
+//   height: 100%;
+//   border-radius: 50px;
+// `
+
+// const ContainerButtons = styled.div`
+//   display: flex;
+//   justify-content: space-around;
+//   align-items: flex-start;
+//   height: 50px;
+//   padding-left: 100px;
+// `
+
+// const ButtonPost = styled.button`
+//   background-color: white;
+//   border-color: blueviolet;
+//   color: blueviolet;
+//   height: 35px;
+//   width: 60px;
+//   border-radius: 6px;
+//   font-size: 14px;
+//   margin-top: 10px;
+//   &:hover {
+//     cursor: pointer;
+//     background-color: white;
+//     border: none;
+//   }
+//   @media screen and (max-width: 1023px) {
+//     width: 60px;
+//   }
+// `
+// const Hide = styled.div`
+//   width: 0.1px;
+//   height: 0.1px;
+//   opacity: 0;
+//   overflow: hidden;
+//   position: absolute;
+//   z-index: -1;
+// `
+
+// const ButtonAdd = styled.label`
+//   background-color: ${colors.secondary};
+//   border-color: ${colors.primary};
+//   color: ${colors.primary};
+//   height: 35px;
+//   width: 60px;
+//   border-radius: 15px;
+//   font-size: 14px;
+//   margin-top: 10px;
+//   margin-right: 80px;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   &:hover {
+//     cursor: pointer;
+//     background-color: white;
+//   }
+//   @media screen and (max-width: 1023px) {
+//     width: 60px;
+//   }
+// `
+// const Close = styled.div`
+//   cursor: pointer;
+//   color: red;
+//   display: flex;
+//   margin: 5px; ;
+// `
+const ButtonPen = styled.div`
+  cursor: pointer;
   &:hover {
     opacity: 0.5;
   }
 `
 
-function modifyOnePost({ postId, setApiCalled }) {
-  //   const [description, setDescription] = useState('')
-  //   const [imageUrl, setImageUrl] = useState('')
-  const token = JSON.parse(localStorage.getItem('token'))
-  //   const Post = (e) => {
-  //     const dataPost = new FormData()
-  //     dataPost.append('description', description)
-  //     dataPost.append('imageUrl', imageUrl)
+// function modifyOnePost({ dataPost, postId, ImageUrlModif, setApiCalled }) {
+//   const token = JSON.parse(localStorage.getItem('token'))
+//   //   const Post = (e) => {
+//   axios({
+//     method: 'PUT',
+//     url: `http://localhost:5000/api/publis/${postId}`,
+//     headers: {
+//       Authorization: `Bearer ${token}`,
+//       'Content-Type': 'application/json',
+//     },
+//     data: dataPost,
+//   })
+//     .then((res) => {
+//       if (res.ok) {
+//         return res.json
+//       }
+//       console.log(res.data)
+//       alert('votre post a bien été modifié')
+//     })
 
-  axios({
-    method: 'PATCH',
-    url: `http://localhost:5000/api/publis/${postId}`,
-    headers: {
-      Authorization: `Bearer ${token}`,
-      'Content-Type': 'application/json',
-    },
-    // data: dataPost,
-  })
-    .then((res) => {
-      if (res.ok) {
-        return res.json
-      }
-      console.log(res.data)
-      alert('votre post a bien été modifié')
-    })
+//     .catch((err) => {
+//       console.log(err)
+//       window.alert("Votre post n'a pas été modifié. Veuillez recommencer!")
+//     })
+// }
 
-    .catch((err) => {
-      console.log(err)
-      window.alert("Votre post n'a pas été modifié. Veuillez recommencer!")
-    })
-}
+// function handleModify({ dataPost, postId, setImageUrlModif, setApiCalled }) {
+//   let res = alert('Votre publication va être modifiée. Confirmation?')
+//   if (res) {
+//     modifyOnePost({
+//       postId,
+//       dataPost,
+//       setApiCalled,
+//     })
+//   }
+// }
 
-function handleModify({ postId, setApiCalled }) {
-  let res = alert('Votre publication va être modifiée. Confirmation?')
-  if (res) {
-    modifyOnePost({ postId, setApiCalled })
-  }
-  return true
-}
-
-const ButtonModify = ({ pseudo, postId, setApiCalled }) => {
+const ButtonModify = ({
+  pseudo,
+  description,
+  imageUrl,
+  setIsModif,
+  postId,
+  setApiCalled,
+}) => {
   let pseudoStock = JSON.parse(localStorage.getItem('pseudo'))
   console.log(pseudo)
+
+  // const [descriptionModif, setDescriptionModif] = useState(description)
+  // const [imageUrlModif, setImageUrlModif] = useState(imageUrl)
+  // const [modifForm, setModifForm] = useState(false)
+
+  // const dataPost = new FormData()
+  // dataPost.append('description', descriptionModif)
+  // dataPost.append('imageUrl', imageUrlModif)
+
   return (
     <Modif
       onClick={(e) => {
         e.preventDefault()
-        handleModify(false)
+        setIsModif(true)
       }}
     >
       {pseudoStock === pseudo ? (
-        <i
-          style={{ fontSize: '100%', padding: '6px' }}
-          class="fa-sharp fa-solid fa-pen-to-square"
-        ></i>
+        <ButtonPen>
+          <i
+            style={{ fontSize: '100%', padding: '6px' }}
+            class="fa-sharp fa-solid fa-pen-to-square"
+          ></i>
+        </ButtonPen>
       ) : (
         <></>
       )}
+      {/* {modifForm === true ? (
+        <>
+          <ContainerCreatePubli>
+            <Close onClick={setModifForm(false)}>
+              <i class="fa-solid fa-xmark"></i>
+            </Close>
+            <FormPost
+              onSubmit={handleModify}
+              id="post"
+              encType="multipart/form-data"
+            >
+              <ContainerPublication>
+                <ProfilImgContainer>
+                  {imageUrl === '/' ? (
+                    <></>
+                  ) : (
+                    <PostImg alt=".jpg" src={imageUrl} />
+                  )}
+                </ProfilImgContainer>
+                <FormStyle>
+                  <InputStyleDescription
+                    id="descriptionInput"
+                    type="text"
+                    placeholder={description}
+                    onChange={(e) => {
+                      const { value } = e.target
+                      setDescriptionModif(value)
+                    }}
+                    required
+                  />
+                  <label
+                    htmlFor="descriptionInput"
+                    style={{ display: 'none' }}
+                  ></label>
+                </FormStyle>
+              </ContainerPublication>
+
+              <ContainerButtons>
+                <Hide>
+                  <input
+                    id="imgUrl"
+                    type="file"
+                    onChange={(e) => {
+                      const imageUrl = e.target.files[0]
+                      setImageUrlModif(imageUrl)
+                    }}
+                  />
+                </Hide>
+                <label htmlFor="imgUrl" alt="Dossier" src=""></label>
+                <ButtonAdd
+                  htmlFor="imgUrl"
+                  type="addPicture"
+                  onClick={() => {}}
+                >
+                  <i className="fa-solid fa-camera"></i>
+                </ButtonAdd>
+                <ButtonPost
+                  type="submit"
+                  id="post"
+                  onClick={() => {
+                    handleModify()
+                    setModifForm(false)
+                  }}
+                >
+                  <ButtonPen>
+                    <i className="fa-solid fa-paper-plane"></i>
+                  </ButtonPen>
+                </ButtonPost>
+                <label htmlFor="post"></label>
+              </ContainerButtons>
+            </FormPost>
+          </ContainerCreatePubli>
+          <div
+            className="description error"
+            style={{
+              fontSize: 16,
+              color: 'red',
+              textAlign: 'center',
+              paddingTop: 2,
+            }}
+          ></div>
+        </>
+      ) : (
+        <></>
+      )} */}
     </Modif>
   )
 }
--- front/src/components/Publi/Card.js
+++ front/src/components/Publi/Card.js
@@ -72,6 +72,102 @@ const PostCreation = styled.div`
   padding-top: 13px;
   margin: 0;
 `
+const ContainerCreatePubli = styled.div``
+const FormPost = styled.form``
+
+const ContainerPublication = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 90%;
+`
+
+const FormStyle = styled.div`
+  border-bottom: solid 2px ${colors.secondary};
+  width: 80%;
+`
+
+const InputStyleDescription = styled.input`
+  border: none;
+  padding: 10px 0px 15px 15px;
+  height: 90%;
+  width: 100%;
+  &::placeholder {
+    font-size: 14px;
+  }
+  &:focus {
+    outline: 1px solid white;
+  }
+`
+
+const PostImg = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 50px;
+`
+
+const ContainerButtons = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: flex-start;
+  height: 50px;
+  padding-left: 100px;
+`
+
+const ButtonPost = styled.button`
+  background-color: white;
+  border-color: blueviolet;
+  color: blueviolet;
+  height: 35px;
+  width: 60px;
+  border-radius: 6px;
+  font-size: 14px;
+  margin-top: 10px;
+  &:hover {
+    cursor: pointer;
+    background-color: white;
+    border: none;
+  }
+  @media screen and (max-width: 1023px) {
+    width: 60px;
+  }
+`
+const Hide = styled.div`
+  width: 0.1px;
+  height: 0.1px;
+  opacity: 0;
+  overflow: hidden;
+  position: absolute;
+  z-index: -1;
+`
+
+const ButtonAdd = styled.label`
+  background-color: ${colors.secondary};
+  border-color: ${colors.primary};
+  color: ${colors.primary};
+  height: 35px;
+  width: 60px;
+  border-radius: 15px;
+  font-size: 14px;
+  margin-top: 10px;
+  margin-right: 80px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &:hover {
+    cursor: pointer;
+    background-color: white;
+  }
+  @media screen and (max-width: 1023px) {
+    width: 60px;
+  }
+`
+const Close = styled.div`
+  cursor: pointer;
+  color: red;
+  display: flex;
+  margin: 5px; ;
+`
 
 const Card = ({
   id,
@@ -87,30 +183,6 @@ const Card = ({
 }) => {
   const [isModif, setIsModif] = useState(false)
 
-  //   const [postUser, setPostUser] = useState()
-
-  //   useEffect(() => {
-  //     // var myHeaders = new Headers()
-  //     // let myInit = {
-  //     //   method: 'GET',
-  //     //   headers: myHeaders,
-  //     //   mode: 'cors',
-  //     //   cache: 'default',
-  //     // }
-
-  //     fetch('http://localhost:5000/api/auth/user')
-  //       .then(function (res) {
-  //         if (res.ok) {
-  //           //   return res.json
-  //           setPostUser(res)
-  //           console.log(res)
-  //         }
-  //       })
-  //       .catch(function (res) {
-  //         console.log('erreur get user')
-  //       })
-  //   }, [postUser])
-  //   console.log(postUser)
   return (
     <CardContainer>
       {/* {setisLoadingCard} ? (
@@ -128,10 +200,14 @@ const Card = ({
             <ButtonModify
               postId={id}
               pseudo={pseudo}
+              description={description}
+              imageUrl={imageUrl}
+              setIsModif={setIsModif}
               setApiCalled={setApiCalled}
               alt="Crayon noir qui écrit"
               onClick={(e) => {
                 e.preventDefault()
+                setIsModif(true)
               }}
             />
             <ButtonSuppr
@@ -144,48 +220,117 @@ const Card = ({
             />
           </ButtonLign>
         </Head>
-        <MDBCard>
-          {imageUrl === '/' ? (
-            <></>
-          ) : (
-            <MDBCardImage
-              src={imageUrl}
-              position="top"
-              alt="photo"
-              style={{ width: '100%' }}
-            />
-          )}
-          <MDBCardBody>
-            <div style={{ padding: '10px', borderBottom: '2px solid #F1F1F1' }}>
-              <MDBCardText>
-                <div>{description}</div>
-                <PostCreation>
-                  Créé il y a {date && dateFormat(new Date(date))}
-                  {/* {Date.now() - date} minutes */}
-                  {/* {dateFormat(new Date(), 'MMM dd yyyy')} */}
-                </PostCreation>
-              </MDBCardText>
-            </div>
-            <span
-              style={{
-                display: 'flex',
-                justifyContent: 'space-around',
-                alignItems: 'center',
-                padding: '10px',
+        {isModif === false ? (
+          <MDBCard>
+            {imageUrl === '/' ? (
+              <></>
+            ) : (
+              <MDBCardImage
+                src={imageUrl}
+                position="top"
+                alt="photo"
+                style={{ width: '100%' }}
+              />
+            )}
+            <MDBCardBody>
+              <div
+                style={{ padding: '10px', borderBottom: '2px solid #F1F1F1' }}
+              >
+                <MDBCardText>
+                  <div>{description}</div>
+                  <PostCreation>
+                    Créé il y a {date && dateFormat(new Date(date))}
+                    {/* {Date.now() - date} minutes */}
+                    {/* {dateFormat(new Date(), 'MMM dd yyyy')} */}
+                  </PostCreation>
+                </MDBCardText>
+              </div>
+              <span
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-around',
+                  alignItems: 'center',
+                  padding: '10px',
+                }}
+              >
+                <MDBBtn href="#" style={{ color: 'blueviolet', fontSize: 18 }}>
+                  {likes}
+                  <i className="fa-regular fa-thumbs-up" />
+                  <i className="fa-solid fa-thumbs-up" />
+                </MDBBtn>
+                <MDBBtn href="#" style={{ color: 'blueviolet', fontSize: 18 }}>
+                  {comments}
+                  <i className="fa-regular fa-comments" />
+                </MDBBtn>
+              </span>
+            </MDBCardBody>
+          </MDBCard>
+        ) : (
+          <MDBCard>
+            <Close
+              onClick={(e) => {
+                setIsModif(false)
               }}
             >
-              <MDBBtn href="#" style={{ color: 'blueviolet', fontSize: 18 }}>
-                {likes}
-                <i className="fa-regular fa-thumbs-up" />
-                <i className="fa-solid fa-thumbs-up" />
-              </MDBBtn>
-              <MDBBtn href="#" style={{ color: 'blueviolet', fontSize: 18 }}>
-                {comments}
-                <i className="fa-regular fa-comments" />
-              </MDBBtn>
-            </span>
-          </MDBCardBody>
-        </MDBCard>
+              <i class="fa-solid fa-xmark"></i>
+            </Close>
+            <FormPost>
+              <ContainerPublication>
+                <ProfilImgContainer></ProfilImgContainer>
+                <FormStyle>
+                  <InputStyleDescription
+                    id="descriptionInput"
+                    type="text"
+                    placeholder={description}
+                    onChange={(e) => {
+                      const { value } = e.target
+                      // setDescriptionModif(value)
+                    }}
+                    required
+                  />
+                  <label
+                    htmlFor="descriptionInput"
+                    style={{ display: 'none' }}
+                  ></label>
+                </FormStyle>
+              </ContainerPublication>
+
+              <ContainerButtons>
+                <Hide></Hide>
+                <label htmlFor="imgUrl" alt="Dossier" src=""></label>
+                <ButtonAdd
+                  htmlFor="imgUrl"
+                  type="addPicture"
+                  onClick={() => {}}
+                >
+                  <i className="fa-solid fa-camera"></i>
+                </ButtonAdd>
+                <ButtonPost
+                  type="submit"
+                  id="post"
+                  onClick={() => {
+                    // handleModify()
+                    setIsModif(false)
+                  }}
+                >
+                  {/* <ButtonPlane> */}
+                  <i className="fa-solid fa-paper-plane"></i>
+                  {/* </ButtonPlane> */}
+                </ButtonPost>
+                <label htmlFor="post"></label>
+              </ContainerButtons>
+            </FormPost>
+            <div
+              className="description error"
+              style={{
+                fontSize: 16,
+                color: 'red',
+                textAlign: 'center',
+                paddingTop: 2,
+              }}
+            ></div>
+          </MDBCard>
+        )}
       </Main>
     </CardContainer>
   )

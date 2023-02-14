bb9e85c5e118a115342ab68d91bc28096014ea89
Céline Sèbe <celine.sebe@gmail.com>
2023-02-14 19:06:01 +0100

    ButtonLike

--- front/src/components/ButtonLike/index.js
+++ front/src/components/ButtonLike/index.js
@@ -1,11 +1,82 @@
-import React from 'react';
+import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
 
-const index = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+const Like = styled.div`
+  opacity: 1;
+`
 
-export default index;
+function handleLike({
+  id,
+  likes,
+  liked,
+  setLiked,
+  setNbLike,
+  usersLikes,
+  token,
+}) {
+  let counter = likes
+  if (liked === false) {
+    counter = counter + 1
+    setLiked(true)
+    setNbLike(counter)
+  } else {
+    counter = counter - 1
+    setLiked(false)
+    setNbLike(counter)
+  }
+  axios
+    .patch(
+      `http://localhost:5000/api/publis/${id}`,
+      { edited_field: 'likes', likes: likes },
+      { edited_field: 'usersLikes', usersLikes: usersLikes },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      }
+    )
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
+
+const ButtonLike = ({ id, likes, usersLikes }) => {
+  const [liked, setLiked] = useState(false)
+  const [nbLike, setNbLike] = useState(likes)
+  //   const [usersLikes, setUsersLikes] = useState()
+
+  const token = JSON.parse(localStorage.getItem('token'))
+
+  return (
+    <>
+      <Like
+        onClick={(e) => {
+          handleLike({
+            id,
+            likes,
+            liked,
+            setLiked,
+            setNbLike,
+            usersLikes,
+            token,
+          })
+        }}
+      >
+        {nbLike > 0 ? nbLike : <></>}
+        {liked === true ? (
+          <i className="fa-solid fa-thumbs-up" />
+        ) : (
+          <i className="fa-regular fa-thumbs-up" />
+        )}
+      </Like>
+    </>
+  )
+}
+
+export default ButtonLike
--- front/src/components/Publi/Card.js
+++ front/src/components/Publi/Card.js
@@ -15,6 +15,7 @@ import {
 } from 'mdb-react-ui-kit'
 import ButtonSuppr from '../ButtonSuppr'
 import ButtonModify from '../ButtonModify'
+import ButtonLike from '../ButtonLike'
 
 const CardContainer = styled.ul`
   display: flex;
@@ -225,9 +226,38 @@ const Card = ({
   comments,
   setApiCalled,
 }) => {
+  // const [isLike, setIsLike] = useState(false)
+  // const [isNbLike, setIsNbLike] = useState()
   const [isModif, setIsModif] = useState(false)
   const [isDescriptionModif, setIsDescriptionModif] = useState(description)
   const [isImageModif, setIsImageModif] = useState(imageUrl)
+  console.log(usersLikes.length)
+
+  // const likeDislike = (e) => {
+  //   const token = JSON.parse(localStorage.getItem('token'))
+
+  //   let counter = likes
+  //   if (isLike === false) {
+  //     counter = counter + 1
+  //     setIsLike(true)
+  //     setIsNbLike(counter)
+  //   } else {
+  //     counter = counter - 1
+  //     setIsLike(false)
+  //     setIsNbLike(counter)
+  //   }
+  //   axios({
+  //     method: 'put',
+  //     url: `http://localhost:5000/api/publis/${id}`,
+  //     headers: {
+  //       Authorization: `Bearer ${token}`,
+  //       'Content-Type': 'application/json',
+  //     },
+  //     data: isNbLike,
+  //   })
+  //     .then((res) => console.log(res))
+  //     .catch((err) => console.log(err))
+  // }
 
   // console.log(isDescriptionModif)
   // console.log(isImageModif)
@@ -301,10 +331,11 @@ const Card = ({
                   padding: '10px',
                 }}
               >
-                <MDBBtn href="#" style={{ color: 'blueviolet', fontSize: 18 }}>
-                  {likes}
-                  <i className="fa-regular fa-thumbs-up" />
-                  <i className="fa-solid fa-thumbs-up" />
+                <MDBBtn
+                  alt="Pouce like"
+                  style={{ color: 'blueviolet', fontSize: 18 }}
+                >
+                  <ButtonLike id={id} likes={likes} />
                 </MDBBtn>
                 <MDBBtn href="#" style={{ color: 'blueviolet', fontSize: 18 }}>
                   {comments}

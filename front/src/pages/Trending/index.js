e237044cc424cc61573efe9c7c9c60b5f253207a
Céline Sèbe <celine.sebe@gmail.com>
2023-06-15 22:37:52 +0200

    css UserList button delete user

--- front/src/pages/Trending/index.js
+++ front/src/pages/Trending/index.js
@@ -1,65 +1,79 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Header from '../../components/Header';
-import TrendingInfo from '../../components/TrendingInfo/TrendingInfo';
-import styled from 'styled-components';
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import Header from '../../components/Header'
+import styled from 'styled-components'
 
-const ContainerTrends= styled.div`
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-margin: 150px;
-
+const TableContainer = styled.div`
+  margin-top: 80px;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  line-height:35px;
+`
+const Close = styled.div`
+  cursor: pointer;
+  color: red;
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 25px;
+  font-size: larger;
 `
 
 function UserList() {
-    
-    const [users, setUsers] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-    
-    useEffect(() => {
-        fetchUsers();
-      }, []);
-    
-      const fetchUsers = () => {
+  const [users, setUsers] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
 
-        let token = JSON.parse(localStorage.getItem('token'))
+  useEffect(() => {
+    fetchUsers()
+  }, [])
 
-        axios({
-            method: 'get',
-            url: 'http://localhost:5000/api/auth/user',
-            headers: {
-              Authorization: `Bearer ${token}`,
-              'Content-Type': 'application/json',
-            },
-          })
-          .then(response => {
-            setUsers(response.data);
-            setIsLoading(false);
-            setError(null);
-            console.log(response)
-          })
-          .catch(error => {
-            setError(error.message);
-            setIsLoading(false);
-          });
-      };
+  const fetchUsers = () => {
+    let token = JSON.parse(localStorage.getItem('token'))
 
-    return (
+    axios({
+      method: 'get',
+      url: 'http://localhost:5000/api/auth/user',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => {
+        setUsers(response.data)
+        setIsLoading(false)
+        setError(null)
+        console.log(response.data)
+      })
+      .catch((error) => {
+        setError(error.message)
+        setIsLoading(false)
+      })
+  }
+
+  return (
     <>
-    <Header />
-   
-     {users.map(user => (
-        <div key={user.id}>
-          <p>{user.name}</p>
-          {/* Afficher d'autres informations d'utilisateur si nécessaire */}
-        </div>
-      ))}
+      <Header />
+      <TableContainer>
+        <table>
+          <caption>Tableau des utilisateurs</caption>
+          {users.map((user) => (
+            <div key={user._id}>
+              <tr>
+                <th>{user.pseudo}</th>
+                <th> : </th>
+                <th>{user.email}</th>
+                <Close>
+                <i class="fa-solid fa-xmark"></i>
+                </Close>
+              </tr>
+              {/* Afficher d'autres informations d'utilisateur si nécessaire */}
+            </div>
+          ))}
+        </table>
+      </TableContainer>
     </>
+  )
+}
 
-    );
-};
-
-export default UserList;
+export default UserList
